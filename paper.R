d44311d37082aae0e7bc64358be31eaa0874aa75
stuartore <stuart.dr@outlook.com>
2024-08-03 18:21:51 +0800

    stu: 已进行分组

--- paper.R
+++ paper.R
@@ -39,33 +39,33 @@ names_mapping <- c(
   "年龄" = "Age", "住院号" = "HospNum", "身高" = "Height", "体重" = "Weight",
   "AHI指数" = "AHI_index", "备注（出院日期）" = "More Notes",
   "糖化血红蛋白" = "Hb1Ac", "白细胞" = "WBC", "中性粒细胞" = "N", "淋巴细胞" = "L",
-    "血红蛋白" = "Hb", "血小板" = "Plt",  "总胆固醇" = "TG", "甘油三酯" = "Triglycerides", 
-    "非HDL胆固醇" = "non-HDL-C",
-    "性别（男1、女0）" = "Sex",
-    "HDL-C" = "HDL",
-    "LDL-C" = "LDL",
-    "载脂蛋白-A" = "Apo-A",
-    "肌酐（尿）" = "Cr-urine",
-    "Sp02" = "SpO2", 
-    "HDL-C" = "HDL_C", 
-    "LDL-C" = "LDL_C", 
-    "载脂蛋白-B" = "Apo-B",
-    "载脂蛋白-E" = "Apo-E",
-    "脂蛋白a" = "Lp(a)", "同型半胱氨酸" = "Homocysteine",  "尿素" = "urea",
-     "肌酐" = "Cr", "尿酸" = "Uric acid", "钾" = "K", "钠" = "Na",  "氯" = "Cl", "总钙" = "Ca2+", "尿微量白蛋白" = "Urine microalbumin",
-    "空腹血糖mmol/L" = "fasting blood sugar", 
-    "24h尿K" = "24h urine K",
-    "24h尿Na" = "24h urine Na",
-    "立位ARR" = "Stand ARR",
-    "卧位ARR" = "Recumbent ARR",
-    "卧位肾素（5点）" = "Recumbent renin 5am", 
-    "立位醛固酮（7点）" = "Orthostatic aldosterone 7am",
-    "立位肾素（7点）" = "orthorenin 7am", 
-    "卧位醛固酮（5点）" = "Recumbent aldosterone 5am", 
-    "血浆皮质醇测定（早上8:00）" = "Cortisol 8am", 
-    "血浆皮质醇测定（下午16:00）" = "Cortisol 16pm",
-    "血浆皮质醇测定（晚上0:00）" = "Cortisol 0am",
-    "促肾上腺皮质激素" = "corticotropin"
+  "血红蛋白" = "Hb", "血小板" = "Plt",  "总胆固醇" = "TG", "甘油三酯" = "Triglycerides", 
+  "非HDL胆固醇" = "non-HDL-C",
+  "性别（男1、女0）" = "Sex",
+  "HDL-C" = "HDL",
+  "LDL-C" = "LDL",
+  "载脂蛋白-A" = "Apo-A",
+  "肌酐（尿）" = "Cr-urine",
+  "Sp02" = "SpO2", 
+  "HDL-C" = "HDL_C", 
+  "LDL-C" = "LDL_C", 
+  "载脂蛋白-B" = "Apo-B",
+  "载脂蛋白-E" = "Apo-E",
+  "脂蛋白a" = "Lp(a)", "同型半胱氨酸" = "Homocysteine",  "尿素" = "urea",
+    "肌酐" = "Cr", "尿酸" = "Uric acid", "钾" = "K", "钠" = "Na",  "氯" = "Cl", "总钙" = "Ca2+", "尿微量白蛋白" = "Urine microalbumin",
+  "空腹血糖mmol/L" = "fasting blood sugar", 
+  "24h尿K" = "24h urine K",
+  "24h尿Na" = "24h urine Na",
+  "立位ARR" = "Stand ARR",
+  "卧位ARR" = "Recumbent ARR",
+  "卧位肾素（5点）" = "Recumbent renin 5am", 
+  "立位醛固酮（7点）" = "Orthostatic aldosterone 7am",
+  "立位肾素（7点）" = "orthorenin 7am", 
+  "卧位醛固酮（5点）" = "Recumbent aldosterone 5am", 
+  "血浆皮质醇测定（早上8:00）" = "Cortisol 8am", 
+  "血浆皮质醇测定（下午16:00）" = "Cortisol 16pm",
+  "血浆皮质醇测定（晚上0:00）" = "Cortisol 0am",
+  "促肾上腺皮质激素" = "corticotropin"
 )
 
 # 定义一个函数来处理名称修改
@@ -82,6 +82,16 @@ data <- modify_names(data, names_mapping)
 
 bio_data <- data[ , !(names(data) %in% c('Index', 'BedNum', 'PatientName', 'HospNum', 'SpO2', 'More Notes'))]
 
+# 分组
+bio_data <- bio_data %>%
+  mutate(`My Group` = case_when(
+    `AHI_index` > 15 & `if_ph` == 1 ~ "PH + OSA（+）",
+    `AHI_index` <= 15 & `if_ph` == 1 ~ "PH + OSA（-）",
+    `AHI_index` > 15 & `if_pa` == 1 ~ "PA + OSA（+）",
+    `AHI_index` <= 15 & `if_pa` == 1 ~ "PA + OSA（-）",
+    TRUE ~ NA_character_ # 为其他情况指定默认值，例如NA
+  ))
+
 bio_data$AHI_group <- cut(bio_data$AHI_index, 
   breaks = c(-Inf, 15, Inf),
   labels = c("OSA(-)", "OSA(+)"),
@@ -94,8 +104,12 @@ bio_data$if_pa <- cut(bio_data$if_pa,
   include.lowest = TRUE
 )
 
+bio_data$if_ph <- cut(bio_data$if_ph, 
+  breaks = c(-Inf, 0.5, Inf),
+  labels = c("Diagnosed PH", "Not Diagnosed PH"),
+  include.lowest = TRUE
+)
+
 cat("\n", names(data))
 
-print(descrTable( AHI_group ~ . , data = bio_data))
-
-
+print(descrTable(`My Group` ~ . , data = bio_data))
